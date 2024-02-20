25f3bbc4daa10823ff285ff0c372c50a47a20096
Claudio Pontes <claudiohpontes@gmail.com>
2024-02-20 12:32:55 -0300

    Adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,5 +1,13 @@
-n01 = int(input('Insira um número inteiro: '))
+#2n01 = int(input('Insira um número inteiro: '))
+#n02 = int(input('Insira um número inteiro: '))
+#print(n01 // n02)
 
-n02 = int(input('Insira um número inteiro: '))
 
-print(n01 // n02)
+import math
+
+raio = float(input("Digite o raio do circulo: "))
+
+
+area_circulo = math.pi * raio ** 2
+
+print(area_circulo)
